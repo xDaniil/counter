bf979b341c6caef623cccfce0d97cbf7bd1f816e
xDaniil <dan.mednikov@icloud.com>
2020-06-09 17:06:44 +0300

    meta

--- counter-test-task/pages/index.tsx
+++ counter-test-task/pages/index.tsx
@@ -1,8 +1,13 @@
 import CounterActions from "../components/counterActions";
+import Head from 'next/head'
 
 const Home = () => {
   return (
     <>
+      <Head>
+        <title>Counter app</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <CounterActions></CounterActions>
     </>
   )
